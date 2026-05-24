075026de2b451e99741c7bfc5fd73debcde9af93
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-23 22:43:09 -0300

    Add comentarios

--- App/Controller/InicialController.php
+++ App/Controller/InicialController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 final class InicialController extends Controller
 {
+    // Rota raiz ("/"). Verifica segurança e carrega a tela principal.
     public static function index() : void
     {
         parent::isProtected();       
 
+        // Usa 'include' direto ao invés de 'parent::render' pois a home pode não necessitar de um Model específico para iniciar.
         include VIEWS . '/Inicial/home.php';
     }
 }
