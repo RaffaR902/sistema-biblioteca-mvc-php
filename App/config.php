e65543cacd0e138b5d2131ed6c62e7c96a55d5df
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-24 16:05:28 -0300

    Modifica config

--- App/config.php
+++ App/config.php
@@ -1,1 +1,9 @@
 <?php
+
+define('BASE_DIR', dirname(__FILE__, 2));
+define('VIEWS', BASE_DIR . '/App/View');
+
+$_ENV['db']['host'] = "localhost:3306";
+$_ENV['db']['user'] = "root";
+$_ENV['db']['pass'] = "47h35Z91%P=:";
+$_ENV['db']['database'] = "biblioteca";
