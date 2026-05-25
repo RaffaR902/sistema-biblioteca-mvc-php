b7b4591098f34d7cd536b2f3bb63264812cdd2ef
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-24 22:17:43 -0300

    Add Model

---
+++ App/Model/Model.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+
+abstract class Model
+{
+    final public array $rows = [];
+
+    private array $errors = [];
+
+
+    final public function setError(string $msg) : void
+    {
+        $this->errors[] = $msg;
+    }
+
+    final public function getErrors() : string
+    {
+        if(empty($this->errors))
+            return "";
+
+        $msg = "<ul>";
+
+        foreach($this->errors as $error)
+            $msg .= "<li>$error</li>";
+
+        $msg .= "</ul>";
+
+        return $msg;
+    }
+}
