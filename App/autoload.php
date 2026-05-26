1f43fe4c63d41c80e5fac30b9612cb1da2e2728a
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-25 22:44:47 -0300

    Add comentarios

--- App/autoload.php
+++ App/autoload.php
@@ -1,13 +1,14 @@
 <?php
 
-// Registra uma função de autoload automática
-// Essa função é chamada sempre que uma classe for utilizada
-// e ainda não estiver carregada no sistema
+/* Registra uma função de autoload automática
+ * Essa função é chamada sempre que uma classe for utilizada e ainda não estiver carregada no sistema
+ */
 spl_autoload_register(function ($nome_da_classe)
 {
-    // Monta o caminho completo do arquivo da classe
-    // Exemplo:
-    // App\Model\Aluno -> BASE_DIR/App/Model/Aluno.php
+    /* Monta o caminho completo do arquivo da classe
+     * Exemplo:
+     * App\Model\Aluno -> BASE_DIR/App/Model/Aluno.php
+     */
     $arquivo = BASE_DIR . "/" . $nome_da_classe . ".php";
 
     // Verifica se o arquivo existe
