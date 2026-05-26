2d974eb5083e1d188af192895c270fdcec4d5e2a
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-25 22:44:57 -0300

    Add comentarios

--- App/config.php
+++ App/config.php
@@ -1,9 +1,24 @@
 <?php
 
+/* Define o diretório base do projeto
+ * dirname(__FILE__, 2) volta 2 níveis de pasta a partir deste arquivo
+ */
 define('BASE_DIR', dirname(__FILE__, 2));
+
+// Define o caminho da pasta de Views do sistema
 define('VIEWS', BASE_DIR . '/App/View');
 
+
+// Configurações de conexão com banco de dados
+
+// Endereço do servidor MySQL
 $_ENV['db']['host'] = "localhost:3306";
+
+// Usuário do banco de dados
 $_ENV['db']['user'] = "root";
-$_ENV['db']['pass'] = "47h35Z91%P=:";
+
+// Senha do banco de dados
+$_ENV['db']['pass'] = "";
+
+// Nome do banco utilizado pelo sistema
 $_ENV['db']['database'] = "biblioteca";
