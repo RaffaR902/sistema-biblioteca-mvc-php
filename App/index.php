c04754416e26cdc0fbc7fefc0cc406302a72a3b7
RaffaR902 <RafaR.RibeiroR@gmail.com>
2026-05-25 22:45:17 -0300

    Add comentarios

--- App/index.php
+++ App/index.php
@@ -1,7 +1,17 @@
 <?php
 
+/* Inicia a sessão do sistema
+ * Necessário para login, autenticação e armazenamento de dados do usuário
+ */
 session_start();
 
+// Carrega o arquivo de configurações do sistema
 include "config.php";
+
+// Carrega o autoload responsável por incluir classes automaticamente
 include "autoload.php";
+
+/* Carrega as rotas da aplicação
+ * Define quais controllers serão executados para cada URL
+ */
 include "routes.php";
